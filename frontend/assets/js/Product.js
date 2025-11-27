339de5dcb642ed3d30ca592f1227765005239660
Ben <benedictcyril5@gmail.com>
2025-11-27 19:32:15 +0530

    Added new img and prd card

---
+++ frontend/assets/js/Product.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.product-card').forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.add('clicked');
+    setTimeout(() => card.classList.remove('clicked'), 300);
+  });
+});
